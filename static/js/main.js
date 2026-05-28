33c0639ebcf18d9faf279e888081b94eba2aa3b7
abdlfc11 <abdul.sultan.dev@gmail.com>
2026-05-28 14:59:36 +0100

    feat(UI): add 2 feature cards (more to come) on the landing page

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,8 @@
 // ###########
 
 const createAccountHeroButton = document.getElementById('hero-create-account-button')
-
+const registerLink = document.getElementById("register-link")
+const loginLink = document.getElementById("login-link")
 
 document.querySelectorAll('.about-group-header-button').forEach(headerButton => {
     headerButton.addEventListener('click', () => {
@@ -22,4 +23,10 @@ document.querySelectorAll('.about-group-header-button').forEach(headerButton => 
 
 if (createAccountHeroButton) {
     createAccountHeroButton.disabled = true
+}
+if (registerLink) {
+    registerLink.disabled = true
+}
+if (loginLink) {
+    loginLink.disabled = true
 }
