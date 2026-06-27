d1e1b21746e0fb0980cad20133012805466fb7b9
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-06-27 19:50:40 +0100

    chore(frontend): Add function in utils.js to format ETA into a string of hrs and mins from seconds as an int

--- static/js/utils.js
+++ static/js/utils.js
@@ -100,6 +100,14 @@ export function calculateEta(distanceKm) {
   return `${etaMinutesRemainder}m`;
 }
 
+export function formatETA(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+
+    if (hours === 0) return `${minutes}m`;
+    return `${hours}h ${minutes}m`;
+}
+
 export function showError(message) {
     const container = document.getElementById("error-toast-container");
 
