5f0e16a56a0e03b2f1ea1d7918ca35a584e8e362
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-07-30 11:43:16 +0100

    feat: Add FastAPI search area endpoint and add Settings Schema (pydantic BaseModel)

---
+++ src/Search/search_fastapi.py
@@ -0,0 +1,88 @@
+#region IMPORTS 
+
+# Core Library Imports
+import traceback
+
+
+# Third Party Library Imports
+import requests
+from fastapi import APIRouter, Depends, HTTPException 
+from fastapi_limiter.depends import RateLimiter
+from pyrate_limiter import Duration, Limiter, Rate
+
+# Local Module Imports
+from src.extensions import service, log_action, rate_limit_exceeded_callback
+from src.Search.search_schemas import SearchModel
+from src.config import Config
+
+
+#endregion
+
+router = APIRouter()
+
+@router.post(
+    '/search/search-area',
+    dependencies=[
+        Depends(
+            RateLimiter(
+                limiter=Limiter(Rate(110, Duration.MINUTE)),
+                callback=rate_limit_exceeded_callback
+            )
+        )
+    ]
+)
+def search_area(
+    data: SearchModel
+):
+
+    try: 
+        search_input = data.search_input
+        query_parameters = {
+            'key': Config.LOCATIONIQ_API_KEY,
+            'q': search_input,
+            'format': 'json',
+            'countrycodes': 'gb'
+        }
+
+        response = requests.get("https://eu1.locationiq.com/v1/search", params=query_parameters)
+        results = response.json()
+
+        if isinstance(results, list) and len(results) > 0:
+
+            first_result = results[0]
+
+            latitude = first_result.get("lat")
+            longitude = first_result.get("lon")
+
+            print(f"Latitude: {latitude}\nLongitude: {longitude}")
+
+            web_mercator_x, web_mercator_y = service.convert_wgs84_to_web_mercator(longitude, latitude)
+
+            coords = [web_mercator_x, web_mercator_y]
+
+            print(coords)
+
+            return {
+                "success": True,
+                "coordinates": coords,
+                "display_name": first_result.get("display_name")
+            }
+        else:
+
+            raise HTTPException(
+                status_code=404,
+                detail={
+                    "success": False,
+                    "message": "Could not find area"
+                }
+            )
+    except Exception:
+        log_action('Searching for Area', False, traceback.format_exc(), None, 'SEARCH_AREA')
+
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "success": False,
+                "message": "Sorry, there was an unexpected error, try again later. "
+            }
+        )
