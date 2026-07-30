bf298fe403acaaf575ce0a3d709f45d287b91303
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-07-30 11:49:36 +0100

    chore: Restructure createRouteCard() function call and add new parameter in importRoute.js

--- static/js/importRoute.js
+++ static/js/importRoute.js
@@ -22,7 +22,7 @@ export async function processImportedRouteFile(file) {
     const form = new FormData();
     form.append("route_file", file);
 
-    const response = await fetch("/import_route_file", {
+    const response = await fetch(window.appConfig.apiImportRouteUrl, {
         method: 'POST',
         body: form
     });
@@ -47,16 +47,23 @@ export function displayImportedRouteCard(data) {
 
     const routeInfo = data.route_info;
     const today = new Date();
-        
+
+    const routeName = routeInfo.route_name
+
     const formattedToday = new Intl.DateTimeFormat('en-GB', {
-    "day": "2-digit",
-    "month": "2-digit",
-    "year": "numeric"
+        "day": "2-digit",
+        "month": "2-digit",
+        "year": "numeric"
     }).format(today);
 
+    const distanceKm = routeInfo.distance_km;
+    const formattedDistanceKm = formatDistance(distanceKm);
     const formattedETA = formatETA(routeInfo.eta_seconds);
+    const formattedElevation = formatElevation(routeInfo.elevation_gain_metres)
 
-    const routeCard = createRouteCard(routeInfo.route_name, formattedToday, routeInfo.distance_km, formattedETA, formatElevation(routeInfo.elevation_gain_metres));
+    
+
+    const routeCard = createRouteCard(routeName, formattedToday, distanceKm, formattedDistanceKm, formattedETA, formattedElevation);
 
     if (allRoutesContainer) allRoutesContainer.insertAdjacentHTML("beforeend", routeCard);            
 }
