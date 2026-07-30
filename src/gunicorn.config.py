31ab055fc813b7c888cfc8a3aeeb9aca2328a3f7
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-07-30 15:17:56 +0100

    chore: Edit gunicorn config to use uvicorn workers and apply finishing touches of refactor

--- src/gunicorn.config.py
+++ src/gunicorn.config.py
@@ -1,10 +1,21 @@
+"""
+This file uses gunicorn to manage uvicorn workers 
+Needed as uvicorn does not support shared memory between workers
+"""
+
 import os
 
 bind = "0.0.0.0:5000"
 workers = int(os.getenv("GUNICORN_WORKERS", "2"))
+
+
+worker_class = "uvicorn.workers.UvicornWorker"
+
+# this ensures shared memory between workers 
 preload_app = True
+
+# Environment and logging
+raw_env = ["LOAD_GRAPH_ON_IMPORT=1"]
 accesslog = "-"
 errorlog = "-"
 loglevel = os.getenv("GUNICORN_LOG_LEVEL", "info")
-
-raw_env = ["LOAD_GRAPH_ON_IMPORT=1"]
