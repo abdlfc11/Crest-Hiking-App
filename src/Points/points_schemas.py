90fe0f5765dfc24dbcf86812f0434fd3527a3901
Abdulrahman Sultan <119352671+abdlfc11@users.noreply.github.com>
2026-08-02 16:25:35 +0100

    chore: Add stronger validation to saved points coordinates

--- src/Points/points_schemas.py
+++ src/Points/points_schemas.py
@@ -1,7 +1,18 @@
-from pydantic import BaseModel
+from typing import Annotated
+import math
+from pydantic import BaseModel, AfterValidator, Field
+
+# better validation for coordinates to ensure that erroneous coordinates are caught before they hit FastAPI routes 
+def check_finite(value: float) -> float:
+    if not math.isfinite(value):
+        raise ValueError("Coordinates must be finite. ")
+    return value
+    
+Longitude = Annotated[float, AfterValidator(check_finite), Field(ge=-180, le=180)]
+Latitude = Annotated[float, AfterValidator(check_finite), Field(ge=-90, le=90)]
 
 
 class PointSchema(BaseModel):
     point_name: str
-    lon: float | None = None
-    lat: float | None = None
+    lon: Longitude | None = None
+    lat: Latitude | None = None
